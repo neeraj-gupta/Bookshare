2cb2571457b31aea980e64ad9027c4dbda8511fe
Sanstuti Srivastav <sanstuti_srivastav@yahoo.co.in>
2015-04-23 16:09:05 -0700

    Student.java changes

--- src/main/java/Model/Student.java
+++ src/main/java/Model/Student.java
@@ -1,5 +1,48 @@
 package Model;
 
 public class Student {
-
+	
+	String studentId;
+	String firstName;
+	String lastName;
+	String email;
+	String phone;
+	String university;
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getUniversity() {
+		return university;
+	}
+	public void setUniversity(String university) {
+		this.university = university;
+	}
+	
 }
