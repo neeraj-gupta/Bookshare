f383449aa24a2e25b9c85991608d3035000acb47
Anurag Bhardwaj <anurag.wizard@gmail.com>
2015-05-05 04:26:51 -0700

    BookDaoImpl Update

--- BookshareBackend/src/main/java/cmpe275Project/DAO/BookDaoImpl.java
+++ BookshareBackend/src/main/java/cmpe275Project/DAO/BookDaoImpl.java
@@ -1,12 +1,14 @@
 package cmpe275Project.DAO;
 
 import java.util.List;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
 import cmpe275Project.Model.Book;
+import cmpe275Project.Model.PostBook;
 import cmpe275Project.MyExceptions.Exceptions;
 import cmpe275Project.config.SpringMongoConfig;
 
@@ -14,6 +16,8 @@ public class BookDaoImpl implements BookDao {
 
 	private static MongoOperations mongoOps;
 	private static final String Book_COLLECTION = "books";  
+	private static final String PostBook_COLLECTION = "postbooks";
+	
 	
 	public BookDaoImpl(){
 		if(mongoOps == null){
@@ -34,6 +38,17 @@ public class BookDaoImpl implements BookDao {
 	}
 	
 	@Override
+	public void postBook(PostBook postBook) {
+		// TODO Auto-generated method stub
+		if(!isExistingPostBook(postBook.getBookId())){
+			postBook.setBookId(postBook.getBookId() + 1);
+		}
+		
+		System.out.println("mongoOps " + mongoOps);
+		mongoOps.insert(postBook, PostBook_COLLECTION);
+	}
+	
+	@Override
 	public Book readBook(Integer b_id) {
 		// TODO Auto-generated method stub
 		Query query = new Query(Criteria.where("_id").is(b_id));
@@ -45,6 +60,18 @@ public class BookDaoImpl implements BookDao {
 		// TODO Auto-generated method stub
 		Query query = new Query(Criteria.where("id").is(bookId));
 		Book existingId = mongoOps.findOne(query, Book.class, Book_COLLECTION);
+
+		if(existingId != null){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private boolean isExistingPostBook(Integer bookId) {
+		// TODO Auto-generated method stub
+		Query query = new Query(Criteria.where("id").is(bookId));
+		Book existingId = mongoOps.findOne(query, Book.class, PostBook_COLLECTION);
 
 		if(existingId != null){
 			return true;
@@ -71,14 +98,35 @@ public class BookDaoImpl implements BookDao {
 	@Override
 	public List<Book> searchBook(String searchkey) {
 		// TODO Auto-generated method stub
-		Query query = new Query(Criteria.where("_id").is(searchkey));
+		Query query = new Query(Criteria.where("bookTitle").is(searchkey));
 		List<Book> book = mongoOps.find(query, Book.class, Book_COLLECTION);
-		return book;
+		if(book.size() != 0){
+			return book;
+		}
+		Query query2 = new Query(Criteria.where("bookAuthor").is(searchkey));
+		List<Book> book2 = mongoOps.find(query2, Book.class, Book_COLLECTION);
+		if(book2.size() != 0){
+			return book2;
+		}
+		Query query3 = new Query(Criteria.where("bookISBN").is(searchkey));
+		List<Book> book3 = mongoOps.find(query3, Book.class, Book_COLLECTION);
+		if(book3.size() != 0){
+			return book3;
+		}
+		return null;
+		
 	}
 
 	@Override
 	public void bidBook(Integer b_id) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public List<Book> listAllBooks(int id) {
+		Query query = new Query(Criteria.where("ownerId").is(id));
+		List<Book> book = mongoOps.find(query, Book.class, Book_COLLECTION);
+		return book;
 	}
 }
