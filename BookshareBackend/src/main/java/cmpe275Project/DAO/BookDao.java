c54032f210f32566f5157c89516dc155ab715e8e
neeraj-gupta <neeraj.gupta@sjsu.edu>
2015-05-04 19:54:02 -0700

    Change Return Type searchBook

--- BookshareBackend/src/main/java/cmpe275Project/DAO/BookDao.java
+++ BookshareBackend/src/main/java/cmpe275Project/DAO/BookDao.java
@@ -1,11 +1,12 @@
 package cmpe275Project.DAO;
 
+import java.util.List;
 import cmpe275Project.Model.Book;
 
 public interface BookDao {
 	public void createBook(Book book);
 	public Book readBook(Integer b_id);
 	public Book deleteBook(Integer b_id);
-	public Book searchBook();
+	public List<Book> searchBook(String searchkey);
 	public void bidBook(Integer b_id);
 }
