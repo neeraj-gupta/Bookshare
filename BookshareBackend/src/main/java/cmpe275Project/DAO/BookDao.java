3e5cf960ecc1fa137cac164fbaa386efccd6f9ff
Anurag Bhardwaj <anurag.wizard@gmail.com>
2015-05-05 04:24:06 -0700

    postBook method add

--- BookshareBackend/src/main/java/cmpe275Project/DAO/BookDao.java
+++ BookshareBackend/src/main/java/cmpe275Project/DAO/BookDao.java
@@ -1,12 +1,16 @@
 package cmpe275Project.DAO;
 
 import java.util.List;
+
 import cmpe275Project.Model.Book;
+import cmpe275Project.Model.PostBook;
 
 public interface BookDao {
 	public void createBook(Book book);
 	public Book readBook(Integer b_id);
 	public Book deleteBook(Integer b_id);
-	public List<Book> searchBook(String searchkey);
+	public List<Book> searchBook(String key);
 	public void bidBook(Integer b_id);
+	void postBook(PostBook postBook);
+	List<Book> listAllBooks(int id);
 }
