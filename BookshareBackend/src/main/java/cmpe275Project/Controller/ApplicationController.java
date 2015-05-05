2e82b97a73596eff1dfc2292b684f4bba4667ccf
neeraj-gupta <neeraj.gupta@sjsu.edu>
2015-05-04 19:58:17 -0700

    update

--- BookshareBackend/src/main/java/cmpe275Project/Controller/ApplicationController.java
+++ BookshareBackend/src/main/java/cmpe275Project/Controller/ApplicationController.java
@@ -3,9 +3,6 @@ package cmpe275Project.Controller;
 import java.security.InvalidParameterException;
 import java.util.*;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import cmpe275Project.DAO.*;
 import cmpe275Project.Model.*;
-import cmpe275Project.config.SpringMongoConfig;
 
 @RestController
 @RequestMapping("/")
