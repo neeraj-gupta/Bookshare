3f7727023fbf93bad467d701b174d7bb505dacad
ashishsjsu <ashish12021@gmail.com>
2015-05-06 03:15:55 -0700

    MySql config update

--- BookshareBackend/src/main/java/cmpe275Project/Util/SqlConfig.java
+++ BookshareBackend/src/main/java/cmpe275Project/Util/SqlConfig.java
@@ -13,14 +13,25 @@ public class SqlConfig {
         else {
         	// MySQL Database Connection.
             try {
-                Properties prop = new Properties();
+               /* Properties prop = new Properties();
                 InputStream inputStream = SqlConfig.class.getClassLoader().getResourceAsStream("/db.properties");
                 prop.load(inputStream);
                 String driver = prop.getProperty("JDBC_Driver");
                 String url = prop.getProperty("DB_URl");
                 String user = prop.getProperty("username");
                 String password = prop.getProperty("password");
-                Class.forName(driver);
+                Class.forName(driver);*/
+                
+                String url = "jdbc:mysql://localhost:3306/BookshareDB";
+                String user = "root";
+                String password = "root";
+                
+                try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
                 connection = DriverManager.getConnection(url, user, password);
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
