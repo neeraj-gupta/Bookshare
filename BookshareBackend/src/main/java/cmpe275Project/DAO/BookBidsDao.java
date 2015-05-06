df4241f846021650a76cd2d77f79c4fdfc56884b
Anurag Bhardwaj <anurag.wizard@gmail.com>
2015-05-06 00:48:44 -0700

    Update BookBidsDao.java

--- BookshareBackend/src/main/java/cmpe275Project/DAO/BookBidsDao.java
+++ BookshareBackend/src/main/java/cmpe275Project/DAO/BookBidsDao.java
@@ -8,13 +8,10 @@ import cmpe275Project.Model.RentOrBuy;
 public interface BookBidsDao {
 	
 	public void addBid(BookBids bookBids);
-
 	public List<BookBids> listBids(int id);
-
 	public boolean bidIdExist(Integer bidId);
-
 	public BookBids getBid(Integer bidId);
-
 	public RentOrBuy getRentOrBuyRecord(Integer bookId);
-
+	
 }
+
