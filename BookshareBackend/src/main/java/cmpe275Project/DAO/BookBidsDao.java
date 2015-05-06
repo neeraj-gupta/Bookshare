399e1b58e616930afe125ded33dfd4eb2351037d
Sanstuti Srivastav <sanstuti_srivastav@yahoo.co.in>
2015-05-05 23:14:54 -0700

    Update bid dao

--- BookshareBackend/src/main/java/cmpe275Project/DAO/BookBidsDao.java
+++ BookshareBackend/src/main/java/cmpe275Project/DAO/BookBidsDao.java
@@ -3,11 +3,18 @@ package cmpe275Project.DAO;
 import java.util.List;
 
 import cmpe275Project.Model.BookBids;
+import cmpe275Project.Model.RentOrBuy;
 
 public interface BookBidsDao {
 	
 	public void addBid(BookBids bookBids);
 
 	public List<BookBids> listBids(int id);
+
+	public boolean bidIdExist(Integer bidId);
+
+	public BookBids getBid(Integer bidId);
+
+	public RentOrBuy getRentOrBuyRecord(Integer bookId);
 
 }
