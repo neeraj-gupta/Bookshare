c5b2b03d3da28e321542ce5ec8a03871fdc7c450
neeraj-gupta <neeraj.gupta@sjsu.edu>
2015-05-06 16:47:34 -0700

    Added one method and updated search

--- BookshareBackend/src/main/java/cmpe275Project/Controller/BookController.java
+++ BookshareBackend/src/main/java/cmpe275Project/Controller/BookController.java
@@ -14,8 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import cmpe275Project.DAO.*;
-import cmpe275Project.Model.Book;
-import cmpe275Project.Model.PostBook;
+import cmpe275Project.Model.*;
 
 @RestController
 @RequestMapping("/")
@@ -27,31 +26,31 @@ public class BookController {
     @RequestMapping( method = RequestMethod.POST, value = "/createbook")
     public @ResponseBody Book createBook(@RequestBody Book book) {
     	
-			Book bookLocal = new Book(student_id, book.getBookTitle(), book.getBookAuthor(), book.getBookISBN(), book.getBookDesc(), book.getBookCondition());
-			checkValidBook(bookLocal);
+	Book bookLocal = new Book(student_id, book.getBookTitle(), book.getBookAuthor(), book.getBookISBN(), book.getBookDesc(), book.getBookCondition());
+	checkValidBook(bookLocal);
 			
-			// Call StudentDao Class method to create Student.
-			bookdao.createBook(bookLocal);
-			System.out.println("1. Book added : " + bookLocal);
-			return bookLocal;
+	// Call StudentDao Class method to create Student.
+	bookdao.createBook(bookLocal);
+	System.out.println("1. Book added : " + bookLocal);
+	return bookLocal;
     }
     
 	// Post for a required Book.
     @RequestMapping( method = RequestMethod.POST, value = "/postbook")
     public @ResponseBody PostBook postForBook(@RequestBody PostBook postBook) {
     	
-    		PostBook postBookLocal = new PostBook(student_id, postBook.getBookTitle(), postBook.getBookAuthor(), postBook.getBookISBN(), postBook.getBookDesc(), postBook.getBookCondition(), postBook.getPrice());
-    		checkValidPostBook(postBookLocal);
-			bookdao.postBook(postBookLocal);
-			System.out.println("1. Book required Posted : " + postBookLocal);
-			return postBookLocal;
+    	PostBook postBookLocal = new PostBook(student_id, postBook.getBookTitle(), postBook.getBookAuthor(), postBook.getBookISBN(), postBook.getBookDesc(), postBook.getBookCondition(), postBook.getPrice());
+    	checkValidPostBook(postBookLocal);
+	bookdao.postBook(postBookLocal);
+	System.out.println("1. Book required Posted : " + postBookLocal);
+	return postBookLocal;
     }
 
-    // List of all available Book.
-    @RequestMapping( method = RequestMethod.GET, value = "/listallbooks/{id}")
-    public ResponseEntity<JSONArray> listBooks(@PathVariable(value = "id")int id) {
+        // List of user Books.
+    @RequestMapping( method = RequestMethod.GET, value = "/listUserbooks/{id}")
+    public ResponseEntity<JSONArray> listUserBooks(@PathVariable(value = "id")String id) {
     	
-    	List<Book> books = bookdao.listAllBooks(id);
+    	List<Book> books = bookdao.listUserBooks(id);
     	JSONArray jsonArray = new JSONArray();
     	for(Book book : books){
     		jsonArray.add(book);
@@ -62,13 +61,35 @@ public class BookController {
     	return new ResponseEntity<JSONArray>(jsonArray, HttpStatus.ACCEPTED);
     }
     
+    // List of all available Book.
+    @RequestMapping( method = RequestMethod.GET, value = "/listallbooks")
+    public ResponseEntity<JSONArray> listallBooks() {
+    	
+    	List<Book> books = bookdao.listAllBooks();
+    	JSONArray jsonArray = new JSONArray();
+    	for(Book book : books){
+    		jsonArray.add(book);
+    	}
+    	
+    	System.out.println("1. All for Listing Found : ");
+    	//return jsonArray;
+    	return new ResponseEntity<JSONArray>(jsonArray, HttpStatus.ACCEPTED);
+    }
     
     // Search for a Book.
     @RequestMapping( method = RequestMethod.GET, value = "/searchbook/{key}")
-    public @ResponseBody List<Book> searchBook(@PathVariable(value = "key")String key) {
+    public @ResponseBody ResponseEntity<JSONArray> searchBook(@PathVariable(value = "key")String key) {
     	List<Book> books = bookdao.searchBook(key);
-		System.out.println("1. Search for a book : " + books);
-		return books;
+	System.out.println("1. Search for a book : " + books);
+		
+	JSONArray jsonArray = new JSONArray();
+    	for(Book book : books){
+    		jsonArray.add(book);
+    	}
+    	
+    	System.out.println("1. All for Listing Found : ");
+    	//return jsonArray;
+    	return new ResponseEntity<JSONArray>(jsonArray, HttpStatus.ACCEPTED);
     }
     
     // Delete a Book.
@@ -79,8 +100,8 @@ public class BookController {
     	if(book.getBookId() != null){
     		bookdao.deleteBook(idInt);
     	}
-		System.out.println("1. Search for a book : " + book);
-		return book;
+	System.out.println("1. Search for a book : " + book);
+	return book;
     }
     
     private void checkValidBook(Book book) {
