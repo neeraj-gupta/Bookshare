936c949c7c49583d159406caea91ac676a338a15
Sanstuti Srivastav <sanstuti_srivastav@yahoo.co.in>
2015-05-05 20:01:28 -0700

    Update in transaction

--- BookshareBackend/src/main/java/cmpe275Project/Controller/RentOrBuyController.java
+++ BookshareBackend/src/main/java/cmpe275Project/Controller/RentOrBuyController.java
@@ -1,41 +1,50 @@
 package cmpe275Project.Controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import cmpe275Project.DAO.RentOrBuyDao;
 import cmpe275Project.DAO.RentOrBuyDaoImpl;
+import cmpe275Project.DAO.TransactionDao;
+import cmpe275Project.DAO.TransactionDaoImpl;
+import cmpe275Project.Model.RentOrBuy;
+import cmpe275Project.Model.Transaction;
 
 @RestController
 @RequestMapping("/")
 public class RentOrBuyController {
 	
 	private RentOrBuyDao rentOrBuyDao = new RentOrBuyDaoImpl();
+	private TransactionDao transactionDao = new TransactionDaoImpl();
+	private int studentId = 101;
 	
-	// Buy a Book. --- >>> Merge Transaction
+		// Buy a Book. --- >>> Merge Transaction --> Hardcoding sold and rent
 	 	@RequestMapping(method = RequestMethod.POST, value = "/buybook")
-	 	public @ResponseBody String buyBook(
-	 			@PathVariable(value = "id") int id)
+	 	public @ResponseBody String buyBook(@RequestBody RentOrBuy rentOrBuy)
 	 			 {
 
 	 		// Validate book
-	 		// checkValidBook(title, author, isbn, sellingPrice);
-
-	 		if (!(rentOrBuyDao.getBookStatus(id).equalsIgnoreCase("sold") || rentOrBuyDao.getBookStatus(id).equalsIgnoreCase("rented"))) {
+	 		
+	 		if (!(rentOrBuyDao.getBookStatus(rentOrBuy.getBookId()).equalsIgnoreCase("sold") || rentOrBuyDao.getBookStatus(rentOrBuy.getBookId()).equalsIgnoreCase("rented"))) {
 
 	 			// Change status in DB
-	 			if ((rentOrBuyDao.changeBookStatus(id, "Sold").equalsIgnoreCase("success"))) {
-	 				System.out.println("Book with id " + id + " bought");
+	 			if ((rentOrBuyDao.changeBookStatus(rentOrBuy.getBookId(), "Sold").equalsIgnoreCase("success"))) //Changes status
+	 			{
+	 				//Check the value of type
+	 				Transaction transaction = new Transaction(rentOrBuy.getBookId(), studentId ,rentOrBuy.getOwnerId(), rentOrBuy.getType(), rentOrBuy.getSellingPrice());
+	 				transactionDao.createTransaction(transaction);
+	 				
+	 				System.out.println("Book with id " + rentOrBuy.getBookId() + " bought");
+	 				System.out.println("Transaction id is: " + transaction.getTransactionId());
 	 			} else {
 	 				System.out.println("Book buying failed, could not update");
 	 			}
 	 		} else {
 	 			System.out.println("Book you are trying to buy has status "
-	 					+ rentOrBuyDao.getBookStatus(id));
+	 					+ rentOrBuyDao.getBookStatus(rentOrBuy.getBookId()));
 	 		}
 
 	 		return "Success";
@@ -43,21 +52,23 @@ public class RentOrBuyController {
 
 	 	// Rent a Book.  --- >>> Merge Transaction
 	 	@RequestMapping(method = RequestMethod.POST, value = "/rentbook")
-	 	public @ResponseBody String rentBook(@PathVariable(value = "id") int id,
-	 			@RequestParam(value = "duration", required = true) String duration) {
+	 	public @ResponseBody String rentBook(@RequestBody RentOrBuy rentOrBuy) {
 	 		
 	 		//Method to validate book
-	 		if (!(rentOrBuyDao.getBookStatus(id).equalsIgnoreCase("Sold") || rentOrBuyDao.getBookStatus(id).equalsIgnoreCase("Rented"))) {
+	 		if (!(rentOrBuyDao.getBookStatus(rentOrBuy.getBookId()).equalsIgnoreCase("Sold") || rentOrBuyDao.getBookStatus(rentOrBuy.getBookId()).equalsIgnoreCase("Rented"))) {
 
 	 			// Change status in DB
-	 			if ((rentOrBuyDao.changeBookStatus(id, "Rented").equalsIgnoreCase("success"))) {
-	 				System.out.println("Book with id " + id + " rented for " + duration + " days");
+	 			if ((rentOrBuyDao.changeBookStatus(rentOrBuy.getBookId(), "Rented").equalsIgnoreCase("success"))) {
+	 				Transaction transaction = new Transaction(rentOrBuy.getBookId(), studentId ,rentOrBuy.getOwnerId(), rentOrBuy.getType(), rentOrBuy.getRentPrice());
+	 				transactionDao.createTransaction(transaction);
+	 				System.out.println("Book with id " + rentOrBuy.getBookId() + " rented for " + rentOrBuy.getDuration() + " days");
+	 				System.out.println("Transaction id is: " + transaction.getTransactionId());
 	 			} else {
 	 				System.out.println("Book buying failed, could not update");
 	 			}
 	 		} else {
 	 			System.out.println("Book you are trying to buy has status "
-	 					+ rentOrBuyDao.getBookStatus(id));
+	 					+ rentOrBuyDao.getBookStatus(rentOrBuy.getBookId()));
 	 		}
 
 	 		
